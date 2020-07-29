7578b1a5120b52dec16e168635a329d59ec40b14
Barış Pınar <58821198+barispinara@users.noreply.github.com>
2020-07-29 16:00:08 +0300

    Add files via upload

---
+++ telegram.php
@@ -0,0 +1,36 @@
+<?php
+include "D:\Jotform\Project 1 (Whatsapp)/JotForm.php";
+$jotformAPI = new JotForm("53f94ff42756396aee1f2159ec9a486d");
+
+$submissions = $jotformAPI->getFormSubmissions("202101164247037");
+
+
+function telegram($msg) {
+    global $telegrambot,$telegramchatid;
+    $url='https://api.telegram.org/bot'.$telegrambot.'/sendMessage';$data=array('chat_id'=>$telegramchatid,'text'=>$msg);
+    $options=array('http'=>array('method'=>'POST','header'=>"Content-Type:application/x-www-form-urlencoded\r\n",'content'=>http_build_query($data),),);
+    $context=stream_context_create($options);
+    $result=file_get_contents($url,false,$context);
+    return $result;
+}
+
+
+$telegrambot= '1296708550:AAFcfS9g3UdqNM6jazSNQbVnVR8v85364uY';
+$telegramchatid=-329405531;
+
+echo "Name ".$submissions[0]["answers"][4]["answer"]["first"];
+echo "\n";
+echo "Surname ".$submissions[0]["answers"][4]["answer"]["last"];
+echo "\n";
+echo "Email ".$submissions[0]["answers"][5]["answer"];
+echo "\n";
+echo "Phone Number ".$submissions[0]["answers"][6]["answer"]["area"];
+echo " ".$submissions[0]["answers"][6]["answer"]["phone"];
+
+telegram("new form submission has been received");
+telegram("Name ".$submissions[0]["answers"][4]["answer"]["first"]);
+telegram("Surname ".$submissions[0]["answers"][4]["answer"]["last"]);
+telegram("Email ".$submissions[0]["answers"][5]["answer"]);
+telegram("Phone Number ".$submissions[0]["answers"][6]["answer"]["area"]." ".$submissions[0]["answers"][6]["answer"]["phone"])
+
+?>
