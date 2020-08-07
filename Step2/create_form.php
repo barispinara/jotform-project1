20251726c551da92f173aa4abc59916aa3476906
Barış Pınar <58821198+barispinara@users.noreply.github.com>
2020-08-07 09:56:26 +0300

    Update create_form.php

--- Step2/create_form.php
+++ Step2/create_form.php
@@ -111,7 +111,7 @@ function create_form(){
         }
         fclose($file);
 
-        $jotformAPI = new JotForm("53f94ff42756396aee1f2159ec9a486d");
+        $jotformAPI = new JotForm("API KEY");
         $response =$jotformAPI->createForm($form);
 }
 
